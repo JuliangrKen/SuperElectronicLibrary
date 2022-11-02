eff6bc6fa9a4914b38818564d85818ee6a760839
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-11-02 16:25:51 +0300

    Создал контекст базы данных для приложения

---
+++ SuperElectronicLibrary/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperElectronicLibrary.Models;
+
+namespace SuperElectronicLibrary
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Book>? Books { get; set; }
+
+        public AppDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS01;" +
+                                        "Initial Catalog=testing;" +
+                                        "Integrated Security=True;" +
+                                        "TrustServerCertificate=True");
+
+        }
+    }
+}
