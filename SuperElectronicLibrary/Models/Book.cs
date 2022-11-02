04d6dbb81f15d6919755820cd8c64b6f2660183e
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-11-02 16:25:32 +0300

    Добавил базовые модели

---
+++ SuperElectronicLibrary/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperElectronicLibrary.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string? Tittle { get; set; }
+        public DateTime YearOfPublication { get; set; }
+    }
+}
