b34fbcbbe7e113991d08f579c6deb56d66532695
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-11-02 17:01:24 +0300

    Добавил два репозитория

---
+++ SuperElectronicLibrary/Repositories/BookRepository.cs
@@ -0,0 +1,51 @@
+﻿using SuperElectronicLibrary.Models;
+
+namespace SuperElectronicLibrary.Repositories
+{
+    public class BookRepository
+    {
+        public List<Book>? GetAll()
+        {
+            using var db = new AppDbContext();
+
+            return db.Books?.ToList();
+        }
+
+        public Book? GetById(int id)
+        {
+            using var db = new AppDbContext();
+
+            return db.Books?.FirstOrDefault(b => b.Id == id);
+        }
+
+        public void Add(params Book[] Book)
+        {
+            using var db = new AppDbContext();
+
+            db.Books?.AddRange(Book);
+            db.SaveChanges();
+        }
+
+        public void Remove(params Book[] Book)
+        {
+            using var db = new AppDbContext();
+
+            db.Books?.RemoveRange(Book);
+            db.SaveChanges();
+        }
+
+        public void UpdateYearOfPublicationById(int id, DateTime newDate)
+        {
+            using var db = new AppDbContext();
+
+            var book = db.Books?.FirstOrDefault(b => b.Id == id);
+
+            if (book is null)
+                throw new Exception();
+
+            book.YearOfPublication = newDate;
+
+            db.SaveChanges();
+        }
+    }
+}
---
+++ SuperElectronicLibrary/Repositories/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using SuperElectronicLibrary.Models;
+
+namespace SuperElectronicLibrary.Repositories
+{
+    public class UserRepository
+    {
+        public List<User>? GetAll()
+        {
+            using var db = new AppDbContext();
+
+            return db.Users?.ToList();
+        }
+
+        public User? GetById(int id)
+        {
+            using var db = new AppDbContext();
+
+            return db.Users?.FirstOrDefault(u => u.Id == id);
+        }
+
+        public void Add(params User[] user)
+        {
+            using var db = new AppDbContext();
+
+            db.Users?.AddRange(user);
+            db.SaveChanges();
+        }
+
+        public void Remove(params User[] user)
+        {
+            using var db = new AppDbContext();
+
+            db.Users?.RemoveRange(user);
+            db.SaveChanges();
+        }
+
+        public void UpdateNameById(int id, string newName)
+        {
+            using var db = new AppDbContext();
+
+            var user = db.Users?.FirstOrDefault(b => b.Id == id);
+
+            if (user is null)
+                throw new Exception();
+
+            user.Name = newName;
+       
+            db.SaveChanges();
+        }
+    }
+}
