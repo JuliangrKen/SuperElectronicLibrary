5d4f8aaa76a87576fecd1cfff23133db5cd361c5
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-11-03 18:58:25 +0300

    Добавил автора в дб сет

--- SuperElectronicLibrary/AppDbContext.cs
+++ SuperElectronicLibrary/AppDbContext.cs
@@ -7,6 +7,7 @@ namespace SuperElectronicLibrary
     {
         public DbSet<User>? Users { get; set; }
         public DbSet<Book>? Books { get; set; }
+        public DbSet<Author>? Authors { get; set; }
 
         public AppDbContext()
         {
