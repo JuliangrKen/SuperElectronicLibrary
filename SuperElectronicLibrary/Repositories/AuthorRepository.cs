f1721eacd1fa00f38d2208a66544b3c25af958eb
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-11-03 19:34:12 +0300

    Добавил репозиторий авторам

---
+++ SuperElectronicLibrary/Repositories/AuthorRepository.cs
@@ -0,0 +1,20 @@
+﻿using SuperElectronicLibrary;
+using SuperElectronicLibrary.Models;
+
+public class AuthorRepository
+{
+    public void Add(params Author[] authors)
+    {
+        using var db = new AppDbContext();
+
+        db.Authors?.AddRange(authors);
+        db.SaveChanges();
+    }
+
+    public Author? GetById(int id)
+    {
+        using var db = new AppDbContext();
+
+        return db.Authors?.FirstOrDefault(b => b.Id == id);
+    }
+}
