60072f759dbfe08eebe5a1f9886ad7f0fe31c6ca
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-11-03 19:59:46 +0300

    Исправил нелепую опечатку

--- SuperElectronicLibrary/AppDbContext.cs
+++ SuperElectronicLibrary/AppDbContext.cs
@@ -8,7 +8,7 @@ namespace SuperElectronicLibrary
         public DbSet<User>? Users { get; set; }
         public DbSet<Book>? Books { get; set; }
         public DbSet<Author>? Authors { get; set; }
-        public DbSet<Genre>? Genre { get; set; }
+        public DbSet<Genre>? Genres { get; set; }
 
         public AppDbContext()
         {
