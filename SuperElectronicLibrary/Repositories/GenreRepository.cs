6ae7ddac37ed60244edc0f9b9a0acafd06f6eb21
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-11-03 20:09:01 +0300

    Добавил репозиторий для работы с жанрами

---
+++ SuperElectronicLibrary/Repositories/GenreRepository.cs
@@ -0,0 +1,40 @@
+﻿using SuperElectronicLibrary.Models;
+
+namespace SuperElectronicLibrary.Repositories
+{
+    public class GenreRepository
+    {
+        public void Add(params Genre[] genres)
+        {
+            using var db = new AppDbContext();
+
+            db.Genres?.AddRange(genres);
+            db.SaveChanges();
+        }
+
+        public Genre? GetById(int id)
+        {
+            using var db = new AppDbContext();
+
+            return db.Genres?.FirstOrDefault(b => b.Id == id);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"> Вызывается, если не найден жанр </exception>
+        public int GetNumBooksByAuthorId(int id)
+        {
+            using var db = new AppDbContext();
+
+            var author = db.Genres?.FirstOrDefault(b => b.Id == id);
+
+            if (author == null)
+                throw new ArgumentException();
+
+            return author.Books.Count();
+        }
+    }
+}
